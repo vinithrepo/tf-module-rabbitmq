175412b3f345da4b34932dd8510c02635954a3b1
Vinith Amma <vinithamma007@gmail.com>
2025-06-28 20:56:21 +0530

    session39 tf

--- userdata.sh
+++ userdata.sh
@@ -2,6 +2,6 @@
 
 yum install ansible -y   &>>/opt/userdata.log
 
-ansible-playbook -i localhost, -U https://github.com/vinithrepo/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/vinithrepo/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
 
 
